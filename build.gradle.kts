7434d03176b73b7b85b1991a9c9dc765d58d1b93
Nuriabl13 <nuria13balderas2l@gmail.com>
2026-04-16 10:24:13 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application{
+    mainClass=""
+}
+javafx{
+    modules("javafx.controls","javafx.fxml")
+    version="21"
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
