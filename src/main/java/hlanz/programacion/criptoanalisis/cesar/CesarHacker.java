bc4477b0292337b0c09d2444f9687759c72a8beb
VVVi888 <vvvictor878@gmail.com>
2026-04-21 09:05:07 +0200

    Corregido problema al importar clases de compañeros

--- src/main/java/hlanz/programacion/criptoanalisis/cesar/CesarHacker.java
+++ src/main/java/hlanz/programacion/criptoanalisis/cesar/CesarHacker.java
@@ -2,6 +2,9 @@ package hlanz.programacion.criptoanalisis.cesar;
 
 import hlanz.programacion.criptoanalisis.HackResult;
 import hlanz.programacion.criptoanalisis.Hackeador;
+import hlanz.programacion.criptoanalisis.Idioma;
+
+import java.util.List;
 
 public class CesarHacker implements Hackeador {
     protected List<Idioma> idiomasPosibles;
