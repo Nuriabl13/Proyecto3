f75191da26665e9bf460ec38f41495960b84ea93
Nuriabl13 <nuria13balderas2l@gmail.com>
2026-04-21 10:00:29 +0200

    Correccion tabulaciones

--- src/main/java/hlanz/programacion/criptoanalisis/util/AnalizadorFrase.java
+++ src/main/java/hlanz/programacion/criptoanalisis/util/AnalizadorFrase.java
@@ -5,15 +5,15 @@ import java.util.List;
 
 public class AnalizadorFrase {
     private String[] palabras;
-public AnalizadorFrase(String frase){
-    this.palabras=frase.split(" ");
-}
-public int getNumeroPalabras(){
-    return  this.palabras.length;
-}
-public List<String> getPalabras(){
-    List<String> listaPalabras = new ArrayList<>();
-    listaPalabras.addAll(List.of(this.palabras));
-    return listaPalabras;
-}
+    public AnalizadorFrase(String frase){
+        this.palabras=frase.split(" ");
+    }
+    public int getNumeroPalabras(){
+        return  this.palabras.length;
+    }
+    public List<String> getPalabras(){
+        List<String> listaPalabras = new ArrayList<>();
+        listaPalabras.addAll(List.of(this.palabras));
+        return listaPalabras;
+    }
 }
